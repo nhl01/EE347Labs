aedaff364421d59b887caa274d9d8096e5ca79dc
Steven Gong <stevegwy@Stevendebijibendiannao.local>
2025-11-07 17:00:15 -0800

    update on the roll pitch yaw and I wrote a function to test the inverse kinematics... completed challenge1

--- Lab3/mecharm_INVK_group_08.py
+++ Lab3/mecharm_INVK_group_08.py
@@ -1,3 +1,4 @@
+import csv
 import numpy as np
 from sympy  import symbols, cos, sin, atan2, pi, Matrix, lambdify 
 from scipy.optimize import least_squares
@@ -44,27 +45,81 @@ forward_kinematics_func = lambdify(q_sym, symbolic_forward_kinematics(q_sym), 'n
 
 def position_error(q_position, x_target, y_target, z_target):
     # Forward kinematics for first 3 links
-    T_values = forward_kinematics_func(q_position[1], q_position[2], q_position[3], 0, 0, 0)
+    T_values = forward_kinematics_func(q_position[0], q_position[1], q_position[2], 0, 0, 0)
     # Extract X, Y, Z values
     X, Y, Z = T_values[:3, 3]
     # Return the extracted minus target positions
     return [X-x_target, Y-y_target, Z-z_target]
 
 def orientation_error(q_orientation, rx_d, ry_d, rz_d):
-    T_values = forward_kinematics_func(0, 0, 0, q_orientation[4], q_orientation[5], q_orientation[6])
+    T_values = forward_kinematics_func(0, 0, 0, q_orientation[0], q_orientation[1], q_orientation[2])
     # Extract X, Y, Z values
-    X, Y, Z = T_values[:3, 3]
-    roll, pitch, yaw = np.arctan2(Z,Y), np.arctan2(-X,np.sqrt(Y*Y+Z*Z)), np.arctan2(Y,X)
+    
+    roll, pitch, yaw = np.arctan2(T_values[2][1], T_values[2][2]), np.arctan2(-1*T_values[2][0], np.sqrt(T_values[0][0]*T_values[0][0]+T_values[1][0]*T_values[1][0])), np.arctan2(T_values[1][0], T_values[0][1] )
     
     return [roll - rx_d, pitch - ry_d, yaw - rz_d]
     
-print(forward_kinematics_func(0, 0, 0, 0, 0, 0))
-
 def inverse_kinematics(x_target, y_target, z_target, rx_d, ry_d, rz_d, q_init, max_iterations=100, tolerance=1e-6):
     position_args = (x_target, y_target, z_target)
-    q_position_solution = least_squares(position_error(q_init, x_target, y_target, z_target), q_init[:3], args=position_args, method='lm', max_nfev=max_iterations, ftol=tolerance).x
+    q_position_solution = least_squares(position_error, q_init[:3], args=position_args, method='lm', max_nfev=max_iterations, ftol=tolerance).x
 
     orientation_args = (rx_d, ry_d, rz_d)
     q_orientation_solution = least_squares(orientation_error, q_init[3:], args=orientation_args, method='lm', max_nfev=max_iterations, ftol=tolerance).x
 
-    return np.concatenate(q_position_solution, q_orientation_solution)
+    return np.concatenate((q_position_solution, q_orientation_solution))
+
+def readCSV():
+    # Load Data
+    filename = "../Lab2/data.csv"
+    angles = []
+    coord = []
+
+    with open(filename, mode="r") as f:
+        reader = csv.reader(f)         
+        i = 0
+        #The csv read comes in this format: ['[11.42', ' 119.35', ' 166.72', ' 133.94', ' -81.56', ' -62.84]']
+        for row in reader:
+            #Remove [ and ] from first index string and last one
+            print()
+            row[0] = row[0].replace('[', '')
+            row[-1] = row[-1].replace(']', '')
+            #Conver String to float
+            temp = []
+            for col in row:
+                temp.append(float(col))
+
+            #even Line of the csv is angle and odd is coord here, because i is 0-based
+            if (i % 2 == 0):
+                angles.append(temp)
+            else:
+                coord.append(temp)
+            i+=1
+    return angles, coord    
+
+# This function will throw the coordinates into the reverse kinmatics and get the angles; next throw that angles in forward kinematics to get the coordinates to compare with our actual
+def testingWithRecordedCoor():
+    angles, coor= readCSV()
+    
+    for i in range(len(angles)):
+        x_target = coor[i][0]
+        y_target = coor[i][1]
+        z_target = coor[i][2]
+        rx_d = np.radians(coor[i][3])  # Roll angle (in radians)
+        ry_d = np.radians(coor[i][4])  # Pitch angle (in radians)
+        rz_d = np.radians(coor[i][5])  # Yaw angle (in radians)
+        q_init = angles[i]
+        # Apply inverse kinematics with given coordinates
+        joint_angles = inverse_kinematics(x_target, y_target, z_target, rx_d, ry_d, rz_d, q_init)
+        # Apply forward kinematics with computed angles
+        cal_coor = symbolic_forward_kinematics(joint_angles)
+        end_effector_position = cal_coor[:3, 3]
+
+        print("Coor:")
+        for j in range(len(end_effector_position)):
+            print("c" + str(j) + ": " + str(end_effector_position[j]))
+
+
+
+testingWithRecordedCoor() # Call this function to test the inverse kinematics
+
+
