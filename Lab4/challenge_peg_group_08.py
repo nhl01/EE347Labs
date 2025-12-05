b61aadebedf59719b23c25a059017d6fc3118fc1
nhl01 <nicholas.h.leung@gmail.com>
2025-12-05 00:36:57 -0800

    Cleanup and comment challenge_peg_group_08

--- Lab4/challenge_peg_group_08.py
+++ Lab4/challenge_peg_group_08.py
@@ -9,30 +9,15 @@ sys.path.append(ROOT_DIR)
 from Lab3.mecharm_INVK_advanced_group_08 import *   # or specific functions
 from Lab2.mecharm_control_group_08 import Read10Position
 
-import csv
 from pymycobot.mycobot import MyCobot
 from pymycobot import PI_PORT, PI_BAUD
 
-mycobot = MyCobot(PI_PORT, PI_BAUD)
+mycobot = MyCobot(PI_PORT, PI_BAUD) #Create MyCobot instantiation
 angles,coords = readCSV("peg.csv")
-readyPick = [coords[0],angles[0]]
-pickPosition = [coords[1], angles[1]]
+readyPick = [coords[0],angles[0]] #Position to hover the arm above the set pickup location
+pickPosition = [coords[1], angles[1]] #Position to pick up a peg
 
-
-def sendRobotToSpecificLocation(angles, coords, readyPick, readyDrop):
-    # Initial our robot arm
-    mycobot = MyCobot(PI_PORT, PI_BAUD)
-    mycobot.power_on()
-    print(readyPick)
-
-    # Set robot to default position with the gripper open
-    sleep(1)
-    mycobot.send_angles([0,0,0,0,0,0],30)
-    print("Start coordinates")
-    sleep(1)
-    mycobot.set_gripper_state(0,30)
-    sleep(2)
-
+#Pick up a peg from the set pickup location and return to neutral position
 def pickUpPeg():
     mycobot.send_coords(getCoordinates(readyPick[0], mycobot.get_angles()), 30)
     print("At ready to pick")
@@ -50,13 +35,12 @@ def pickUpPeg():
     print("Back to neutral")
     sleep(2)
 
-def placePeg(stageAngle, stageCoord, placeAngle, placeCoord):
-    mycobot.send_coords(getCoordinates(stageCoord, mycobot.get_angles()), 30) #getCoordinates(stageCoord, mycobot.get_angles()), 30
-    #mycobot.send_angles(stageAngle, 30)
+#Place a peg into its slot by hovering the peg above the slot (stageCoord) then moving down into position (placeCoord). Returns back to neutral position
+def placePeg(stageCoord, placeCoord):
+    mycobot.send_coords(getCoordinates(stageCoord, mycobot.get_angles()), 30)
     print("in stage position")
     sleep(3)
     mycobot.send_coords(getCoordinates(placeCoord, mycobot.get_angles()), 10)
-    #mycobot.send_angles(placeAngle, 50)
     print("In PLACE position")
     sleep(7)
     mycobot.set_gripper_state(0,30)
@@ -72,7 +56,7 @@ def placePeg(stageAngle, stageCoord, placeAngle, placeCoord):
     print("Back to neutral")
     sleep(3)
 
-
+#Performs inverse kinematics to get solution joint angles, and recalculates solution coordinates using our own forward kinematics
 def getCoordinates(coords, angles):
     x_target = coords[0]
     y_target = coords[1]
@@ -88,6 +72,7 @@ def getCoordinates(coords, angles):
     # Send coordinates to robot
     return end_effector_position
 
+#Function to pick and place three pegs
 def the_Challenge():
     # Initial our robot arm
     mycobot.power_on()
@@ -101,19 +86,20 @@ def the_Challenge():
     print("gripper open")
     sleep(3)
 
+    #Pick up and place 3 pegs
     pickUpPeg()
-    placePeg(angles[2], coords[2], angles[3], coords[3])
+    placePeg(coords[2], coords[3])
     mycobot.set_gripper_state(0,30)
     print("gripper open")
     sleep(1)
     pickUpPeg()
-    placePeg(angles[4], coords[4], angles[5], coords[5])
+    placePeg(coords[4], coords[5])
     mycobot.set_gripper_state(0,30)
     print("gripper open")
     sleep(1)
     pickUpPeg()
-    placePeg(angles[6], coords[6], angles[7], coords[7])
-    mycobot.send_angles([0,0,0,0,0,0],30) #Temporary here
+    placePeg(coords[6], coords[7])
+    mycobot.send_angles([0,0,0,0,0,0],30) #End at neutral
 
 def testPlace():
     mycobot.power_on()
@@ -123,15 +109,15 @@ def testPlace():
     mycobot.send_angles([0,0,0,0,0,0],30)
     print("Start coordinates")
     sleep(2)
-    mycobot.set_gripper_state(0,30)
+    mycobot.set_gripper_state(0,30) #Open gripper to grab a peg
     print("gripper open")
     sleep(3)
-    mycobot.set_gripper_value(40,30,1)
+    mycobot.set_gripper_value(40,30,1) #Grab the peg
     print("Closed gripper")
     sleep(2)
-    placePeg(angles[4], coords[4], angles[5], coords[5])
+    placePeg(coords[4], coords[5]) #Set of peg place coordinates to test
 
 #Read10Position() #Uncomment this to drag teach
 
-#testPlace()
-the_Challenge()
+#testPlace() #Uncomment this to test placing a single peg into its slot starting from neutral
+the_Challenge() #Uncomment this to run Lab 4 Challenge 1
